5baa5ea809c66ea63270b1761532ad7f2ffa5d99
Guy Brown <guybrown78@gmail.com>
2019-03-06 09:42:17 +0000

    updted project card to increase picture

--- src/js/components/ProjectCategoryCard.js
+++ src/js/components/ProjectCategoryCard.js
@@ -10,34 +10,89 @@ class Button extends React.Component {
   }
 }
 
-
-class CardHeader extends React.Component {
+class InfoButton extends React.Component {
   render() {
-    const { image, category, id, categoryDisplayName } = this.props;
-    var style = { 
-        backgroundImage: `url(./project-imgs/${category}/${id}/${encodeURI(image)})`
-    };
     return (
-      <header style={style} className="card-header">
-        <h4 className="card-header--title">{categoryDisplayName}</h4>
-      </header>
+			<button 
+				className="button button-secondary"
+				onMouseOver={this.props.mouseOverHandler}
+				onMouseOut={this.props.mouseOutHandler}
+			>
+        <i className="fas fa-info-circle"></i>
+      </button>
     )
   }
 }
 
 
+
+class CardInfo extends React.Component {
+	constructor(props) {
+    super(props);
+		this.mouseOverHandler = this.mouseOverHandler.bind(this);
+		this.mouseOutHandler = this.mouseOutHandler.bind(this);
+		this.state = {
+			isOpen:false
+		}
+	}
+	mouseOverHandler(e){
+		e.preventDefault();
+		this.setState({
+      isOpen: true,
+		});
+	}
+	mouseOutHandler(e){
+		e.preventDefault();
+		this.setState({
+			isOpen: false,
+		});
+	}
+  render() {
+		const { title, project, site } = this.props;
+    return (
+			<div 
+				className={'card-info ' + (this.state.isOpen ? 'open' : 'closed')}
+			>
+        <h2>{title}</h2>
+        <p className="info-content">
+          {project}
+        </p>
+        <p className="project-site">
+          <i className="fas fa-map-marker-alt"></i> {site}
+        </p>
+        
+				<InfoButton 
+					mouseOutHandler={this.mouseOutHandler}
+					mouseOverHandler={this.mouseOverHandler}
+				/>
+      </div>
+    )
+  }
+}
+
+class CardHeader extends React.Component {
+  render() {
+    const { image, category, id, title } = this.props;
+    var style = { 
+        backgroundImage: `url(./project-imgs/${category}/${id}/${encodeURI(image)})`
+    };
+    return (
+      <div style={style} className="card-header">
+        <h4 className="card-header--title">{title}</h4>
+				<div className="card-header--middle" />
+				<CardInfo 
+					project={this.props.project} 
+					site={this.props.site}
+				/>
+      </div>
+    )
+  }
+}
+
 class CardBody extends React.Component {
   render() {
     return (
       <div className="card-body">
-        <h2>{this.props.title}</h2>
-        <p className="body-content">
-          {this.props.project}
-        </p>
-        <p className="project-site">
-          <i className="fas fa-map-marker-alt"></i> {this.props.site}
-        </p>
-        
         <Button />
       </div>
     )
@@ -48,20 +103,21 @@ class CardBody extends React.Component {
 class ProjectCategoryCard extends React.Component {
   render() {
     const itemURL = `./project-item.html?c=${this.props.category}&id=${this.props.details.id}`;
-    //const clickHandler = () => this.props.onClick(this.props.details.id);
-    return (
-    <a href={itemURL} className="card" /*onClick={clickHandler}*/>
-        <CardHeader 
+		return (
+			<a 
+				href={itemURL}
+				className="card" 
+			>
+       	<CardHeader 
           category={this.props.category} 
-          categoryDisplayName={this.props.categoryDisplayName}
+					title={this.props.details.title} 
+					project={this.props.details.project} 
+          site={this.props.details.site}
           image={this.props.details.image} 
           id={this.props.details.id}
         />
-        <CardBody 
-          title={this.props.details.title} 
-          project={this.props.details.project} 
-          site={this.props.details.site}
-        />
+				<CardBody /> 
+				
       </a>
     )
   }
